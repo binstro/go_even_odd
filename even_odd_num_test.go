0911c90f27c266b2b63188a29235b605db42fb48
Rizki <rizki@expertbook-PH.local>
2022-06-13 09:27:07 +0700

    add bachmark test

--- even_odd_num_test.go
+++ even_odd_num_test.go
@@ -11,6 +11,12 @@ func TestEvenSuccess(t *testing.T) {
 	assert.Equal(t, "ganjil, genap, ganjil, genap, ganjil", result)
 }
 
+func BenchmarkTestEvenSuccess(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		CheckEvenOdd(1, 2, 3, 4, 5)
+	}
+}
+
 func TestEvenOddWithTable(t *testing.T) {
 	tests := []struct {
 		name     string
@@ -36,6 +42,34 @@ func TestEvenOddWithTable(t *testing.T) {
 		t.Run(test.name, func(t *testing.T) {
 			result := CheckEvenOdd(test.request...)
 			assert.Equal(t, test.expected, result)
+		})
+	}
+}
+
+func BenchmarkTestEvenOddWithTable(b *testing.B) {
+	benchmarks := []struct {
+		name     string
+		request  []int
+		expected string
+	}{
+		{
+			name:     "Check Genap",
+			request:  []int{0, 2, 4},
+			expected: "genap, genap, genap",
+		}, {
+			name:     "Check Ganjil",
+			request:  []int{1, 3, 5},
+			expected: "ganjil, ganjil, ganjil",
+		}, {
+			name:     "Check Genap Ganjil",
+			request:  []int{1, 2, 3, 4, 5},
+			expected: "ganjil, genap, ganjil, genap, ganjil",
+		},
+	}
+
+	for _, benchmark := range benchmarks {
+		b.Run(benchmark.name, func(b *testing.B) {
+			CheckEvenOdd(benchmark.request...)
 		})
 	}
 }
